dd1384cf6343182c0a2d810f48e3924bf12c484a
Rudolf16 <mnsnrudolf@gmail.com>
2022-08-10 21:46:23 +0300

    Add chat layout and wrap home page in hoc

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { HomeLayout } from "../components/layout/homeLayout";
-
-export default function Home() {
+import { UnAuthHoc } from "../hocs/UnAuthHoc";
+function Home() {
   return <HomeLayout />;
 }
+export default UnAuthHoc(Home);
